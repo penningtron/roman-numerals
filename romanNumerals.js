959d52d977149b1f93dbb1b95beac86dfc913db7
nathan pennington <nathantp@gmail.com>
2024-01-31 15:35:37 -0600

    doing stuff

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,4 +1,17 @@
 function toRomanLazy(num) {
+
+  let output = "";
+
+  let romanNumeralToArabic = {
+    "I": 1,
+    "V" : 5,
+    "X" : 10,
+    "L" : 50,
+    "C" : 100,
+    "D" : 500,
+    "M" : 1000 
+
+  }
   return "";
 }
 
